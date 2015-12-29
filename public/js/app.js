1aca63c5d89ff8350dabe691fbd3358978d9e643
Cattalyya Nuengsigkapian <cattalyya@hotmail.com>
2015-12-29 16:19:56 -0500

    Add form of chat

--- public/js/app.js
+++ public/js/app.js
@@ -11,3 +11,16 @@ socket.on('message', function (message) {
 	console.log('New message: ');
 	console.log(message.text);
 });
+
+// Handle submitting of new message
+var $form = jQuery('#message-form'); // jQuery(selector => tag of CSS)
+// $ = store jQuery instance of element => this var has ability to access the jQuery instance
+
+// name of event
+$form.on('submit', function (event) {
+	event.preventDefault(); // handle form on your own
+	socket.emit('message', {
+		text: $form.find('input[name=message]').val() //find by tag[attribute]
+	});
+	$form.find('input[name=message]').val('');
+});
