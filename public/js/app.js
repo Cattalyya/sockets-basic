be8d8a34bb9e57c71119673cf6ce66c0b3dd4945
Cattalyya Nuengsigkapian <cattalyya@hotmail.com>
2015-12-29 16:30:42 -0500

    Add message to browser UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,26 @@
 var socket = io();
 
-
+/////////// Connection ///////////
 socket.on('connect',function (){
 // successfully connected on front end part
 // console.log to the JavaScript Console in Developer tools
 	console.log('Connected to socket.io server');
 });
 
+
+/////////// Messages come in ///////////
 socket.on('message', function (message) {
 	console.log('New message: ');
 	console.log(message.text);
+	jQuery('.messages').append('<p>'+ message.text +'</p>') // append = add content inside that tag
 });
 
+
+/////////// Submit message ///////////
 // Handle submitting of new message
 var $form = jQuery('#message-form'); // jQuery(selector => tag of CSS)
 // $ = store jQuery instance of element => this var has ability to access the jQuery instance
 
-// name of event
 $form.on('submit', function (event) {
 	event.preventDefault(); // handle form on your own
 	socket.emit('message', {
